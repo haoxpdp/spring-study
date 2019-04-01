8012ea76380858e67e411f22ea82488ff30e49d3
haoxpT470p <haoxpdp@gmail.com>
2019-04-01 17:32:08 +0800

    init

--- src/main/java/c/hao/spring/study/Main.java
+++ src/main/java/c/hao/spring/study/Main.java
@@ -3,7 +3,12 @@ package c.hao.spring.study;
 import c.hao.spring.study.bean.Car;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.HierarchicalBeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -17,8 +22,12 @@ public class Main {
     public static void main(String[] args) {
         Resource resource = new ClassPathResource("beans.xml");
         BeanFactory factory = new XmlBeanFactory(resource);
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:beans.xml");
+        System.out.println("beans inflate success!");
         Car car = (Car) factory.getBean("car1");
         System.out.println(car.getColor());
+        Car car2 = (Car) context.getBean("car1");
+        System.out.println(car2.getColor());
 
     }
 }
